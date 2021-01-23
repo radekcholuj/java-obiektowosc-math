c0edadfb07ebb360001f37193d5c602c615b3a77
Radoslaw Choluj <radekcholuj@wp.pl>
2021-01-23 15:47:12 +0100

    #1 wyswietlenie liczb randomowych, znalezienie wartosci min i max, wyswietlenie logarytmu z liczby

---
+++ src/MathExample.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class MathExample {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter a number: ");
+        int size = scanner.nextInt();
+
+        NumberRepo numberRepo = new NumberRepo(size);
+        numberRepo.generateValues();
+        numberRepo.displayAll();
+
+        double min = numberRepo.findMin();
+        System.out.println("Min value: "+min);
+
+        double max = numberRepo.findMax();
+        System.out.println("Max value: "+max);
+
+        double[] logNumbers = numberRepo.logNumbers();
+        System.out.println("Log numbers: ");
+        for (double number : logNumbers) {
+            System.out.println(number);
+        }
+    }
+}
